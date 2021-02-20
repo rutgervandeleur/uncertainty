5cf9a85b81de18a74a8d5c994a8b7c506d7e3df3
Jeroen Vranken <jeroen@wcw-stud-145-109-9-41.wireless.uva.nl>
2021-02-20 13:08:01 +0100

    CI-test

--- tests/test_model_init.py
+++ tests/test_model_init.py
@@ -1,25 +1,32 @@
 from pytorch_lightning import Trainer, seed_everything
-import ecg_uncertainty
-from ecg_uncertainty.project.systems.ecgresnet_varinf import ECGResNetVariationalInferenceSystem
+from argparse import ArgumentParser
+import json
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../project'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
-def test_lit_classifier():
+# import unittest
+from systems.ecgresnet_ensemble import ECGResNetEnsembleSystem
+
+def test_model_init():
     seed_everything(1234)
 
     parser = ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
     temp_args, _ = parser.parse_known_args()
 
-    parser = ECGResNetVariationalInferenceSystem.add_model_specific_args(parser)
+    parser = ECGResNetEnsembleSystem.add_model_specific_args(parser)
     args = parser.parse_args()
 
-    ECGResNet_params = json.load(open('ecgresnet_config.json', 'r'))
+    ECGResNet_params = json.load(open('project/ecgresnet_config.json', 'r'))
 
     merged_dict = {**vars(args), **ECGResNet_params}
 
-    model = ECGResNetVariationalInferenceSystem(**merged_dict)
-    assert model.model_name == 'varinf_none'
-#     trainer = Trainer(limit_train_batches=50, limit_val_batches=20, max_epochs=2)
-#     trainer.fit(model, train, val)
+    model = ECGResNetEnsembleSystem(**merged_dict)
 
-#     results = trainer.test(test_dataloaders=test)
-#     assert results[0]['test_acc'] > 0.7
+    assert isinstance(model, ECGResNetEnsembleSystem)
+
+if __name__=='__main__':
+    test_model_init()
+
