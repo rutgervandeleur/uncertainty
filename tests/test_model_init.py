fbc5f211307c6f399a5a4b3885859a995076c99f
Jeroen Vranken <jeroen@wcw-stud-145-109-9-41.wireless.uva.nl>
2021-02-20 13:51:52 +0100

    Implemented EnsembleSystem testcase

--- tests/test_model_init.py
+++ tests/test_model_init.py
@@ -2,31 +2,36 @@ from pytorch_lightning import Trainer, seed_everything
 from argparse import ArgumentParser
 import json
 import sys, os
+import unittest
+
+# Add project directory to path
 testdir = os.path.dirname(__file__)
 srcdir = '../project'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
-
-# import unittest
 from systems.ecgresnet_ensemble import ECGResNetEnsembleSystem
 
-def test_model_init():
-    seed_everything(1234)
+class TestModelInits(unittest.TestCase):
+    def test_ensemble_init(self):
+        seed_everything(1234)
+        parser, ECGResNet_params = get_args()
+        parser = ECGResNetEnsembleSystem.add_model_specific_args(parser)
+        args = parser.parse_args()
+        merged_dict = {**vars(args), **ECGResNet_params}
 
+        model = ECGResNetEnsembleSystem(**merged_dict)
+
+        self.assertTrue(isinstance(model, ECGResNetEnsembleSystem))
+
+def get_args():
     parser = ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
     temp_args, _ = parser.parse_known_args()
+    file = open('project/ecgresnet_config.json', 'r')
+    ECGResNet_params = json.load(file)
+    file.close()
 
-    parser = ECGResNetEnsembleSystem.add_model_specific_args(parser)
-    args = parser.parse_args()
-
-    ECGResNet_params = json.load(open('project/ecgresnet_config.json', 'r'))
-
-    merged_dict = {**vars(args), **ECGResNet_params}
-
-    model = ECGResNetEnsembleSystem(**merged_dict)
-
-    assert isinstance(model, ECGResNetEnsembleSystem)
+    return parser, ECGResNet_params
 
 if __name__=='__main__':
-    test_model_init()
+    unittest.main()
 
